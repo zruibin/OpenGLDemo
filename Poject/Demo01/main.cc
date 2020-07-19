26d5aefa999c5d577d88afe2f722fde09288dde6
ruibin.chow <ruibin.chow@qq.com>
2020-07-19 18:34:06 +0800

    glad使用库方式

--- Poject/Demo01/main.cc
+++ Poject/Demo01/main.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 
-#include "glad/glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
